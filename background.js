a98873570450b2c83ae6ad9eda71d2842cb87e21
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-19 02:32:45 +0530

    fixed chart

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
-console.log("Hello world")
-chrome.runtime.sendMessage({ action: 'start' });
+chrome.runtime.sendMessage({ action: "start" });
 let his = JSON.parse(localStorage.getItem("myEmissionHistory"));
-let duration = localStorage.getItem('duration');
-let stats = localStorage.getItem('stats')
-let arr = JSON.parse(his[0])
+let duration = localStorage.getItem("duration");
+let stats = localStorage.getItem("stats");
+let arr = JSON.parse(his[0]);
 
 // calculate total data transferred
 let history = JSON.parse(his[0]);
@@ -12,78 +11,78 @@ console.log(history);
 let totalData = 0;
 
 for (const property in history) {
-   totalData += history[property];
+  totalData += history[property];
 }
 
+function buildTable(data) {
+  var table = document.getElementById("mynewtable");
+  for (const property in history) {
+    console.log(history);
+    var row = `<tr class="text-gray-700 dark:text-gray-400">
+                    <td class="px-4 py-3">
+                      <div class="flex items-center text-sm">
+                        
+                        <div>
+                          <p class="font-semibold">${property}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-4 py-3 text-sm">
+                      TO be Added in grams
+                    </td>
+                    <td class="px-4 py-3 text-xs">
+                      <span
+                        class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
+                      >
+                        Approved
+                      </span>
+                    </td>
+                    <td class="px-4 py-3 text-sm">
+                      ${history[property]} B
+                    </td>
+                  </tr>`;
+    table.innerHTML += row;
+  }
+}
+
+buildTable(arr);
 // Pie Chart
-let data = {
-    labels: [],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [],
-        backgroundColor: [],
-        hoverOffset: 4
-    }]
+let pdata = {
+  labels: [],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [],
+      backgroundColor: [],
+      hoverOffset: 4,
+    },
+  ],
 };
 
 function randomColor() {
-    let color = 'rgb(';
-    for (let i = 0; i < 3; i++) {
-        let num = Math.floor(Math.random() * 256);
-        color = color.concat(String(num));
-        if (i < 2) {
-            color = color.concat(', ');
-        }
+  let color = "rgb(";
+  for (let i = 0; i < 3; i++) {
+    let num = Math.floor(Math.random() * 256);
+    color = color.concat(String(num));
+    if (i < 2) {
+      color = color.concat(", ");
     }
-    color = color.concat(')');
-    return color;
+  }
+  color = color.concat(")");
+  return color;
 }
 
 for (const property in history) {
-    data["labels"].push(property);
-    data["datasets"][0]["data"].push(history[property]);
-    data["datasets"][0]["backgroundColor"].push(randomColor());
+  pdata["labels"].push(property);
+  pdata["datasets"][0]["data"].push(history[property]);
+  pdata["datasets"][0]["backgroundColor"].push(randomColor());
 }
 
-console.log(data)
-
-function buildTable(data) {
-    var table = document.getElementById("mynewtable");
-    for (const property in history) {
-        console.log(history);
-        var row = `<tr class="text-gray-700 dark:text-gray-400">
-                      <td class="px-4 py-3">
-                        <div class="flex items-center text-sm">
-                          
-                          <div>
-                            <p class="font-semibold">${property}</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        TO be Added in grams
-                      </td>
-                      <td class="px-4 py-3 text-xs">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
-                        >
-                          Approved
-                        </span>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        ${history[property]} B
-                      </td>
-                    </tr>`;
-        table.innerHTML += row;
-     }
-     
-
-  }
-  
-buildTable(arr);
+//console.log(data)
 
 let roundedoff = Math.round(totalData / (1024 * 1024));
-document.getElementById("Overall_footprint").innerHTML = "Carbon Emission  = " + String(roundedoff) + " gm";
+document.getElementById("Overall_footprint").innerHTML =
+  "Carbon Emission  = " + String(roundedoff) + " gm";
 
 console.log(arr);
 console.log(his);
@@ -93,4 +92,3 @@ console.log("Total Data : " + totalData);
 
 //document.getElementById("totalfootprint").innerText = String(getdata());
 //document.getElementById("Overall_footprint").innerHTML = "Footprint (gm) = 5000";
-
