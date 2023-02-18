b2feeb65662d63b1cbd3ff2ada105edb1df6fa46
Bharat Naik <bharatnaik777@gmail.com>
2023-02-19 01:06:58 +0530

    calculated total data transferred

--- background.js
+++ background.js
@@ -5,11 +5,21 @@ let duration = localStorage.getItem('duration');
 let stats = localStorage.getItem('stats')
 let arr = JSON.parse(his[0])
 
+// calculate total data transferred
+let history = JSON.parse(his[0]);
+console.log(history);
+
+let totalData = 0;
+
+for (const property in history) {
+   totalData += history[property];
+}
+
+console.log(Math.round(totalData / (1024 * 1024)));
+
 console.log(arr);
 console.log(his);
 console.log(duration);
 document.getElementById("durationtotal").innerText = duration + " Minutes";
 //document.getElementById("Overall_footprint").innerHTML = "Footprint (gm) = 5000";
-
-
 
