3d73b04fb3d575da5cbed74b1d548371ef4e4f49
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-19 00:55:49 +0530

    added ids

--- background.js
+++ background.js
@@ -3,25 +3,13 @@ chrome.runtime.sendMessage({ action: 'start' });
 let his = JSON.parse(localStorage.getItem("myEmissionHistory"));
 let duration = localStorage.getItem('duration');
 let stats = localStorage.getItem('stats')
+let arr = JSON.parse(his[0])
 
-
-
+console.log(arr);
 console.log(his);
 console.log(duration);
 document.getElementById("durationtotal").innerText = duration + " Minutes";
+//document.getElementById("Overall_footprint").innerHTML = "Footprint (gm) = 5000";
 
-console.log(stats);
 
-let total;
-
-//var obj = {a: 1, b: 2};
-for (var key in his[0][0]) {
-  if (his.hasOwnProperty(key)) {
-    var val = his[key];
-    //console.log("this is the val" +val);
-    total += Number(val);
-    //console.log(val);
-  }
-}
-console.log(total);
 
