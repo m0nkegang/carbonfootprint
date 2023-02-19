e16c8f8faa276a6d156a6c19238ffc47de6b4a1b
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-19 17:39:51 +0530

    added chart support

--- background.js
+++ background.js
@@ -110,18 +110,36 @@ function buildTable(data) {
 
 buildTable(arr);
 // Pie Chart
+
 let pdata = {
-  labels: [],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [],
-      backgroundColor: [],
-      hoverOffset: 4,
-    },
-  ],
+  labels : [],
+  data : [],
+  backgroundColor : []
 };
 
+
+const pieConfig = {
+  type: 'doughnut',
+  data: {
+    datasets: [
+      {
+        data: pdata.data,
+
+        backgroundColor: pdata.backgroundColor,
+        label: 'Dataset 1',
+      },
+    ],
+    labels: pdata.labels,
+  },
+  options: {
+    responsive: true,
+    cutoutPercentage: 80,
+
+    legend: {
+      display: false,
+    },
+  },
+}
 function randomColor() {
   let color = "rgb(";
   for (let i = 0; i < 3; i++) {
@@ -137,9 +155,15 @@ function randomColor() {
 
 for (const property in history) {
   pdata["labels"].push(property);
-  pdata["datasets"][0]["data"].push(history[property]);
-  pdata["datasets"][0]["backgroundColor"].push(randomColor());
+  pdata["data"].push(history[property]);
+  pdata["backgroundColor"].push(randomColor());
 }
+
+// change this to the id of your chart element in HMTL
+const pieCtx = document.getElementById('pie')
+window.myPie = new Chart(pieCtx, pieConfig)
+
+chart
 
 //console.log(data)
 
--- dashboard/assets/js/charts-pie.js
+++ dashboard/assets/js/charts-pie.js
@@ -1,16 +1,11 @@
-/**
- * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
- */
+
 const pieConfig = {
   type: 'doughnut',
   data: {
     datasets: [
       {
         data: [33, 33, 33],
-        /**
-         * These colors come from Tailwind CSS palette
-         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-         */
+
         backgroundColor: ['#0694a2', '#1c64f2', '#7e3af2'],
         label: 'Dataset 1',
       },
@@ -20,10 +15,7 @@ const pieConfig = {
   options: {
     responsive: true,
     cutoutPercentage: 80,
-    /**
-     * Default legends are ugly and impossible to style.
-     * See examples in charts.html to add your own legends
-     *  */
+
     legend: {
       display: false,
     },
